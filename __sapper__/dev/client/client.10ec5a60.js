9af7b41f188c1e1277e70565db12b75b1380f856
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-29 20:32:31 +1000

    refactoring pages after fixing the navbar aligning

---
+++ __sapper__/dev/client/client.10ec5a60.js
@@ -0,0 +1,2 @@
+import './client.2fa84886.js';
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpZW50LjEwZWM1YTYwLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
---
+++ __sapper__/dev/client/client.b9ab6924.js
@@ -0,0 +1,2 @@
+import './client.4608c8a7.js';
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpZW50LmI5YWI2OTI0LmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
